34f7b3d7157dbee9a57d72811a0d5e12c48637b9
Brian V <hokiebrian@gmail.com>
2023-03-19 19:51:53 -0400

    .

--- custom_components/uptime_robot_stats/__init__.py
+++ custom_components/uptime_robot_stats/__init__.py
@@ -1,5 +1,6 @@
 """The uptime_robot component."""
 DOMAIN = "uptime_robot_stats"
+CONF_API_KEY = "api_key"
 
 async def async_setup(hass, config):
     """Set up the uptime_robot component."""
@@ -12,6 +13,8 @@ async def async_setup(hass, config):
 
 async def async_setup_entry(hass, entry):
     """Set up the uptime_robot component from a config entry."""
+    hass.data.setdefault(DOMAIN, {})
+    hass.data[DOMAIN][entry.entry_id] = entry.data[CONF_API_KEY]
     hass.async_create_task(
         hass.config_entries.async_forward_entry_setup(entry, "sensor")
     )
